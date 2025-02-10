a682c6fd58c79c2a5873596084e5b16cadb4c3d3
Rudi Setiyawan <diphons@gmail.com>
2025-02-10 12:32:39 +0700

    kernel: builtin Xiaomi hwid Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -2302,7 +2302,7 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 /* module_blacklist is a comma-separated list of module names */
 static char *module_blacklist;
 static char *custom_module_blacklist[] = {
-	"zram","zsmalloc"
+	"hwid","zram","zsmalloc"
 };
 static bool blacklisted(const char *module_name)
 {
